4f40395322d6481c04ec6ec91a7c319d8f4e2e8f
Вячеслав Форофонов <DanielResult1@outlook.com>
2022-09-12 16:18:14 +0300

    Добавьте файлы проекта.

---
+++ Homework_26/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbersArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        Console.WriteLine("Массив чисел:");
+
+        foreach (var numbers in numbersArray)
+        {
+            Console.Write($"{numbers} ");
+        }
+
+        Console.WriteLine("\nНасколько чисел хотите сдвинуть массив? 1-10");
+        int numbersShift = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < numbersShift; i++)
+        {
+            for (int j = numbersArray.Length - 2; j > -1; j--)
+            {
+                int tempArray = numbersArray[j + 1];
+                numbersArray[j + 1] = numbersArray[j];
+                numbersArray[j] = tempArray;
+            }
+        }
+
+        Console.WriteLine("Новый массив: ");
+
+        foreach (var numbers in numbersArray)
+        {
+            Console.Write($"{numbers} ");
+        }
+    }
+}
